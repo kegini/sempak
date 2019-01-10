de0b43be500e981d140b52cbe1a6207dcdfc598e
Biru Saurus <birusaurus@gmail.com>
2019-01-10 20:12:34 +0700

    delete unused code

--- lib/main.dart
+++ lib/main.dart
@@ -34,23 +34,6 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-// class AfterSplash extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return new Scaffold(
-//       appBar: new AppBar(
-//           title: new Text("Welcome In SplashScreen Package"),
-//           automaticallyImplyLeading: false),
-//       body: new Center(
-//         child: new Text(
-//           "Succeeded!",
-//           style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 class AfterSplash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
