339f9c079e1d04ce6c714e3103d5885257e3cfd9
Biru Saurus <birusaurus@gmail.com>
2019-01-10 20:12:05 +0700

    add image assets, add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,57 @@
 import 'package:flutter/material.dart';
 import 'page/home/page_home.dart';
+import 'package:splashscreen/splashscreen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+        seconds: 3,
+        navigateAfterSeconds: new AfterSplash(),
+        title: new Text(
+          'SemPAK',
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+        ),
+        image: new Image(
+          image: AssetImage("images/logo.png"),
+        ),
+        backgroundColor: Colors.green,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        onClick: () => print("Flutter Egypt"),
+        loaderColor: Colors.white);
+  }
+}
+
+// class AfterSplash extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//       appBar: new AppBar(
+//           title: new Text("Welcome In SplashScreen Package"),
+//           automaticallyImplyLeading: false),
+//       body: new Center(
+//         child: new Text(
+//           "Succeeded!",
+//           style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class AfterSplash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
